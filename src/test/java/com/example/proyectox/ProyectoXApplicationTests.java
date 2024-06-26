d01727e379cd048163258b884539e70803b8bc67
black <blacknegocio3@gmail.com>
2024-06-26 14:43:16 -0500

    Initial commit

---
+++ src/test/java/com/example/proyectox/ProyectoXApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.proyectox;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProyectoXApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
